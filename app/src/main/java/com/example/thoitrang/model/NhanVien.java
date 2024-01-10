5200b063d708716433740d2f52bb2bf11aca4a81
project-html_css <you@example.com>
2024-01-10 11:05:51 +0700

    first commit

---
+++ app/src/main/java/com/example/thoitrang/model/NhanVien.java
@@ -0,0 +1,23 @@
+package com.example.thoitrang.model;
+
+public class NhanVien {
+    public int maNV;
+    public String hoTenNV;
+    public String tenDN;
+    public String matKhau;
+    public String sdtNV;
+    public byte[] hinhNV;
+
+    public NhanVien() {
+    }
+
+    public NhanVien(int maNV, String hoTenNV, String tenDN, String matKhau, String sdtNV, byte[] hinhNV) {
+        this.maNV = maNV;
+        this.hoTenNV = hoTenNV;
+        this.tenDN = tenDN;
+        this.matKhau = matKhau;
+        this.sdtNV = sdtNV;
+        this.hinhNV = hinhNV;
+    }
+
+}
---
+++ app/src/main/java/com/example/thoitrang/model/Top.java
@@ -0,0 +1,14 @@
+package com.example.thoitrang.model;
+
+public class Top {
+    public String tenGiay;
+    public int slTOP;
+
+    public Top() {
+    }
+    public Top(String tenGiay, int slTOP) {
+        this.tenGiay = tenGiay;
+        this.slTOP = slTOP;
+    }
+
+}
